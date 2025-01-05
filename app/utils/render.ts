b0c4eb4a86d8034084082f16c54121f402a0fb0a
Mikasa33 <mikasa33@qq.com>
2025-01-05 22:10:54 +0800

    feat: 修改为使用 nuxt icon

--- app/utils/render.ts
+++ app/utils/render.ts
@@ -1,3 +1,5 @@
-export function renderIcon(cls: string) {
-  return () => h('div', { class: cls })
+import { Icon } from '#components'
+
+export function renderIcon(props: string | { name: string, mode?: 'svg' | 'css', size?: number | string }) {
+  return () => h(Icon, isString(props) ? { name: props } : props)
 }
